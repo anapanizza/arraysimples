7888bcf9da074f1fe55bb604123bb9de4ff5671f
Aluno <aluno@etec.sp.gov.br>
2019-06-06 11:23:16 -0300

    array simples

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//Crindo uma variavel que contenha um array.
+$alunos = array("Brian", "Camila", "Ana", "Douglas");
+
+// Mostrando os valores do array conforme o valor do indice.
+echo $alunos[0]; //mostrara Brian
+echo "br />"; //apenas para quebrar a linha 
+echo $alunos[2]; //mostrara Ana
