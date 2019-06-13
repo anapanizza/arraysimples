095979359837bd446e58896ad82617982a398f40
Aluno <aluno@etec.sp.gov.br>
2019-06-13 11:32:31 -0300

    atualizando index

--- index.php
+++ index.php
@@ -7,3 +7,22 @@ $alunos = array("Brian", "Camila", "Ana", "Douglas");
 echo $alunos[0]; //mostrara Brian
 echo "br />"; //apenas para quebrar a linha 
 echo $alunos[2]; //mostrara Ana
+
+//Adicionando um item ao array:
+$alunos[] = "Elisa";
+
+//Novo: função var_dump para descobrir o que há dentro de uma variável:
+echo "<hr />"; //apenas para criar uma linha separando os dados de cima.
+
+var_dump($alunos);
+
+echo "<hr />"; //apenas para criar uma linha separando os dados de cima.
+
+$logins = array( array("aluno", "123"), //0
+                 array("tiago", "etec"), //1
+                 array("user", "pass") //2
+                 );
+
+//var_dump($logins);
+
+echo $logins[2][0]; // mostra user
